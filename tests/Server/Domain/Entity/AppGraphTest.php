3d915f18be8fd8bee87297e7849af794b99353cc
Baptiste Langlade <baptouuuu@gmail.com>
2019-01-20 16:02:01 +0100

    add app graph section

---
+++ tests/Server/Domain/Entity/AppGraphTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Profiler\Server\Domain\Entity;
+
+use Innmind\Profiler\Server\Domain\{
+    Entity\AppGraph,
+    Entity\Section\Identity,
+    Model\Svg,
+};
+use PHPUnit\Framework\TestCase;
+
+class AppGraphTest extends TestCase
+{
+    public function testInterface()
+    {
+        $section = new AppGraph(
+            $identity = $this->createMock(Identity::class),
+            $svg = new Svg('<svg></svg>')
+        );
+
+        $this->assertSame($identity, $section->identity());
+        $this->assertSame($svg, $section->graph());
+    }
+}
