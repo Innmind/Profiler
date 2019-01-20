e2cefb2a2402f183a00e886eec64532b62c49c1d
Baptiste Langlade <baptouuuu@gmail.com>
2019-01-20 16:01:14 +0100

    add request/response section

---
+++ src/Server/Domain/Entity/RequestResponse.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Profiler\Server\Domain\Entity;
+
+use Innmind\Profiler\Server\Domain\Entity\Section\Identity;
+
+final class RequestResponse
+{
+    private $identity;
+    private $request;
+    private $response;
+
+    private function __construct(Identity $identity, string $request)
+    {
+        $this->identity = $identity;
+        $this->request = $request;
+    }
+
+    public static function received(Identity $identity, string $request)
+    {
+        return new self($identity, $request);
+    }
+
+    public function identity(): Identity
+    {
+        return $this->identity;
+    }
+
+    public function request(): string
+    {
+        return $this->request;
+    }
+
+    public function response(): string
+    {
+        return $this->response;
+    }
+
+    public function respondedWith(string $response): void
+    {
+        $this->response = $response;
+    }
+
+    public function hasRespondedYet(): bool
+    {
+        return \is_string($this->response);
+    }
+}
